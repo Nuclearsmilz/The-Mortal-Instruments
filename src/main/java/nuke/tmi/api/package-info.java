0d05acec136920700575a5034d520d000f8b62d0
Jon Marien <nuclearsmilz@gmail.com>
2017-06-22 17:43:57 -0400

    Removed API

--- src/main/java/nuke/tmi/api/package-info.java
+++
@@ -1,5 +0,0 @@
-@API(owner = References.MODID, provides = References.MODID + "| API", apiVersion = References.APIVERSION)
-package nuke.tmi.api;
-
-import net.minecraftforge.fml.common.*;
-import nuke.tmi.client.*;
